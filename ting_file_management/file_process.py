bce33f6d2cc82943aedcb95b172daf285b80347f
alexandrebelem21 <alexandrebelem21@gmail.com>
2023-06-21 00:29:04 -0300

    Feat: Testando o req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,26 @@
+import sys
+from queue import Queue
+from file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_name = path_file.split('/')[-1]
+
+    for item in instance.items:
+        if item['nome_do_arquivo'] == file_name:
+            return
+
+    lines = txt_importer(path_file)
+
+    if lines:
+        file_data = {
+            "nome_do_arquivo": file_name,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines
+        }
+        instance.enqueue(file_data)
+
+        print(file_data)
 
 
 def remove(instance):
